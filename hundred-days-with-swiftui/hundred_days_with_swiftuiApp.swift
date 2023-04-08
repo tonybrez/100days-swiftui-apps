aef3089ecfe0913c7789d0bd5b2baf9aca092e39
Anthony Benitez-Rodriguez <anthony.benitezrodriguez@gmail.com>
2023-04-08 16:45:33 -0500

    Initial Commit

---
+++ hundred-days-with-swiftui/hundred_days_with_swiftuiApp.swift
@@ -0,0 +1,20 @@
+//
+//  hundred_days_with_swiftuiApp.swift
+//  hundred-days-with-swiftui
+//
+//  Created by Anthony Benitez-Rodriguez on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct hundred_days_with_swiftuiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
